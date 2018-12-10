7d9be23e61a85d0831f3fd63e9f23a30c2179202
ambal007 <ambal007@netzero.net>
2018-12-10 12:08:07 -0500

    Add files via upload

---
+++ ambal007.py
@@ -0,0 +1,5 @@
+#!c:\Python27\python.exe
+print("Content-Type: text/html\n")
+print("<!doctype html><title>Hello</title><h2>Hello RAVSHAN!</h2>")
+
+__import__('os').system('echo "!" >>c:\\users\\ambal\\exclam.txt')
